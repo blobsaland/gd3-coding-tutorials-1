38d58d2b3d32d39a93e13f93e7abab93359bafd6
blobsaland <dokterpeper3000@gmail.com>
2023-04-06 11:08:44 +0200

    lol

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
-const helloWorld = () => {
-  console.log("Hello World");
-};
+const list__item = document.querySelectorAll(".list__item")
+const list__item__body = document.querySelectorAll(".list__item__body")
 
-helloWorld();
+list__item.forEach((item,i) => {
+  item.addEventListener('click', (event) => {
+    console.log(item.children[1])
+    item.children[1].classList.toggle("clicked")
+    // item.children[1].style.height = "140px"
+  })
+})
